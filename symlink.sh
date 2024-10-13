483e68f9304a2abe050c1b145be44efd11eada5f
mbarnesdev <martinbarnesweb@gmail.com>
2024-10-13 15:08:24 +0100

    feat(global): symlink bash script

---
+++ symlink.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DOTFILES_DIR=~/dotfiles
+
+create_symlink() {
+    SRC="$1"
+    DEST="$2"
+
+    if [ -e "$DEST" ]; then
+        rm -rf "$DEST"
+    fi
+
+    ln -s "$SRC" "$DEST"
+}
+
+create_symlink "$DOTFILES_DIR/i3" ~/.config/i3
+create_symlink "$DOTFILES_DIR/nvim" ~/.config/nvim
+create_symlink "$DOTFILES_DIR/tmux" ~/.config/tmux
+create_symlink "$DOTFILES_DIR/wezterm" ~/.config/wezterm
