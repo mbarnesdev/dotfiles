227bf28b649c3cc32fb9e79ddb526e4c95de3a10
mbarnesdev <martinbarnesweb@gmail.com>
2024-10-13 14:37:19 +0100

    feat(nvim): transparency to theme

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -5,5 +5,11 @@ return {
 	opts = {},
 	config = function()
 		vim.cmd.colorscheme "tokyonight-night"
+
+		vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "NonText", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "LineNr", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE" })
 	end,
 }
