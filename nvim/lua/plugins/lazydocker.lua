46d5173f844db63fc60a096b64270738c69a2be9
mbarnesdev <martinbarnesweb@gmail.com>
2024-10-14 12:35:08 +0100

    feat(nvim): lazydocker

---
+++ nvim/lua/plugins/lazydocker.lua
@@ -0,0 +1,11 @@
+return {
+	"crnvl96/lazydocker.nvim",
+	event = "VeryLazy",
+	opts = {},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+	},
+	keys = {
+		{ "<leader>ld", "<cmd>LazyDocker<cr>", desc = "LazyDocker" },
+	},
+}
